95224175be5f33beb2dc4f177c61ef8d363df11f
Yousef <yahmed1996a@gmail.com>
2019-11-25 16:17:26 +0000

    List displayed

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import Vue from'vue';
 document.addEventListener('DOMContentLoaded', ()=>{
   new Vue({
     el:"#app",
-    
+    data:{
+      tasks:["Dishes","Mail Letter"],
+      newTask:""
+    }
   });
 });
