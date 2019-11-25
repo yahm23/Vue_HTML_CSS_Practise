ad79b46776ac4a102b75562bc7db04cab9ca0f8b
Yousef <yahmed1996a@gmail.com>
2019-11-25 16:25:38 +0000

    List can take new tasks

--- src/app.js
+++ src/app.js
@@ -3,9 +3,20 @@ import Vue from'vue';
 document.addEventListener('DOMContentLoaded', ()=>{
   new Vue({
     el:"#app",
+
     data:{
       tasks:["Dishes","Mail Letter"],
       newTask:""
+    },
+
+    methods:{
+      saveTask:function(){
+        this.tasks.push(this.newTask);
+        this.newTask = ""
+      }
+
     }
+
   });
+
 });
